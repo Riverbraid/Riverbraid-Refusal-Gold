6cad62563e3f8b385ed9c9c4bb302c722e5dbf8c
Riverbraid <michael.tilk@gmail.com>
2026-04-14 07:41:20 +0000

    seal: triple-tier integrity floor v1.5.1

--- run-vectors.cjs
+++ run-vectors.cjs
@@ -1,3 +1,10 @@
+((env) => {
+  const WHITELIST = ['PATH','GPG_TTY','HOME','USER','LANG'];
+  Object.keys(env).forEach(key => {
+    if (!WHITELIST.includes(key)) delete env[key];
+  });
+  env.NODE_NO_WARNINGS = '1';
+})(process.env);
 #!/usr/bin/env node
 const crypto = require('crypto');
 const fs = require('fs');
